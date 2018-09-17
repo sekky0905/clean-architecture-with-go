6771c65a1bd9090a16ef66a22ca6bbfd4de73c9c
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-09-17 22:33:43 +0900

    ProgrammingLangのRepositoryを実装した

---
+++ server/domain/repository/programming_lang_repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"github.com/SekiguchiKai/clean-architecture-go/server/domain/model"
+)
+
+// ProgrammingLangRepository は、ProgrammingLangのRepository。
+type ProgrammingLangRepository interface {
+	List(limit int) ([]*model.ProgrammingLang, error)
+	Create(lang *model.ProgrammingLang) (*model.ProgrammingLang, error)
+	Read(id int) (*model.ProgrammingLang, error)
+	ReadByName(name string) (*model.ProgrammingLang, error)
+	Update(lang *model.ProgrammingLang) (*model.ProgrammingLang, error)
+	Delete(id int) error
+}
