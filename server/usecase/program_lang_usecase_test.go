cd7b5a2c519332cc788edac8ccd3613796654c5a
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-09-25 17:23:17 +0900

    ProgrammingLangUseCase.Createのエラーハンドリングを修正した

--- server/usecase/program_lang_usecase_test.go
+++ server/usecase/program_lang_usecase_test.go
@@ -265,7 +265,7 @@ func TestProgrammingLangUseCase_Create(t *testing.T) {
 			want: lang,
 			readWant: readWant{
 				result: nil,
-				err: &model.NoSuchDataError{
+				err: &model.AlreadyExistError{
 					Name:      lang.Name,
 					ModelName: model.ModelNameProgrammingLang,
 				},
